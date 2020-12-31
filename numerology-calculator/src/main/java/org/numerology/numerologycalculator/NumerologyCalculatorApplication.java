25b6ddb473edf7b950e9d11c29e170c956d13fce
shantonu sarker <sarker.shantonu@gmail.com>
2020-12-31 21:48:59 +0600

    init commit

---
+++ numerology-calculator/src/main/java/org/numerology/numerologycalculator/NumerologyCalculatorApplication.java
@@ -0,0 +1,13 @@
+package org.numerology.numerologycalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@SpringBootApplication
+public class NumerologyCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NumerologyCalculatorApplication.class, args);
+	}
+
+}
