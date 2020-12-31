90a3beda00168e18e94ad40f4319d70a99b006c0
shantonu sarker <sarker.shantonu@gmail.com>
2020-12-31 21:48:47 +0600

    init commit

---
+++ numerology-calculator/src/test/java/org/numerology/numerologycalculator/NumerologyCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.numerology.numerologycalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class NumerologyCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
